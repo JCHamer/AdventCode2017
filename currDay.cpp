f63f0c330f3b53a6dd2800a1dfbbf83b42b6e42e
jchamer <jchamer@umich.edu>
2017-12-06 11:58:40 -0500

    partial day 6

--- currDay.cpp
+++ currDay.cpp
@@ -19,21 +19,46 @@ int main(int argc, char *argv[])
 	vector<uint> buckets;
 
 	ifstream infile(argv[1]);
-	string line;
-	while (getline(infile, line))	{
-		stringstream stream(line);
-		while(stream)	{
-			uint num;
-			stream >> num;
-			buckets.push_back(num);
-		}
+	uint num;
+	while (infile >> num)	{
+		buckets.push_back(num);
 	}
 
 	double acc = 0;
 	for (uint x = 0; x < buckets.size(); x++)
 		acc += buckets[x] * (pow(2,(buckets.size() - x - 1)));
+	prevStates.push_back(acc);
 
-	cout << "Total Sum = " << sum << endl;
+	while (true)	{
+		uint pos = 0;
+		for (uint i = 0; i < buckets.size(); i++)	{
+			if (buckets[i] > buckets[pos])
+				pos = i;
+		}
+
+		uint numToDist = buckets[pos++];
+		for (; numToDist > 0; numToDist--)	{
+			if (pos >= buckets.size())
+				pos = 0;
+			buckets[pos] = numToDist;
+		}
+
+		double acc = 0;
+		for (uint x = 0; x < buckets.size(); x++)
+			acc += buckets[x] * (pow(2,(buckets.size() - x - 1)));
+		bool match = false;
+		for (double x : prevStates)	{
+			if (x == acc)
+				match = true;
+		}
+		if (match)
+			break;
+		prevStates.push_back(acc);
+		numSteps++;
+
+	}
+
+	cout << "Total number of steps = " << numSteps << endl;
 
 	return 0;
 }
